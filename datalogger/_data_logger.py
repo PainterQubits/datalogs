abfbc6644b7fb9c085fce7819777157bb04762df
Alex Hadley <alexhad6@gmail.com>
2023-06-30 16:02:13 -0700

    Continue developing DataLogger classes

--- datalogger/_data_logger.py
+++ datalogger/_data_logger.py
@@ -1,38 +1,124 @@
-"""Data logging functions."""
+"""Data logging classes."""
 
+from __future__ import annotations
 from typing import Any
 import os
+import time
 import xarray as xr
 from paramdb import ParamDB
 
 
 class DataLogger:
-    """Logger for saving experiment and analysis data."""
+    """
+    Base data logger to generate :py:class:`GraphDataLogger` objects that use the given
+    ParamDB and log directory.
+    """
 
     def __init__(self, param_db: ParamDB[Any], log_directory: str) -> None:
-        self.param_db = param_db
-        self.log_directory = log_directory
-        if not os.path.exists(log_directory):
-            os.mkdir(log_directory)
+        self._param_db = param_db
+        self._log_directory = log_directory
+        if not os.path.exists(self.directory):
+            os.mkdir(self.directory)
+
+    def _commit_id_or_latest(
+        self, param_db: ParamDB[Any], commit_id: int | None
+    ) -> int:
+        """
+        If the given commit ID is None, return the latest commit ID from the given ParamDB.
+        Otherwise, return the given commit ID.
+        """
+        if commit_id is None:
+            latest_commit = param_db.latest_commit
+            if latest_commit is None:
+                raise IndexError(
+                    "cannot tag log with most recent commit because ParamDB at"
+                    f" '{self._param_db.path}' is empty"
+                )
+            commit_id = latest_commit.id
+        return commit_id
+
+    @property
+    def directory(self) -> str:
+        """Directory where this logger saves files."""
+        return self._log_directory
+
+    def graph_logger(
+        self, graph_description: str, graph_commit_id: int | None = None
+    ) -> GraphDataLogger:
+        """
+        Generate a :py:class:`GraphDataLogger` with the given description and commit ID,
+        or the latest commit ID if one is not provided.
+        """
+        return GraphDataLogger(
+            self._param_db, self._log_directory, graph_description, graph_commit_id
+        )
+
+
+class GraphDataLogger(DataLogger):
+    def __init__(
+        self,
+        param_db: ParamDB[Any],
+        log_directory: str,
+        graph_description: str,
+        graph_commit_id: int | None = None,
+    ) -> None:
+        self._graph_description = graph_description
+        self._graph_commit_id = self._commit_id_or_latest(param_db, graph_commit_id)
+        super().__init__(param_db, log_directory)
+
+    @property
+    def graph_name(self) -> str:
+        return f"graph_{self._graph_commit_id}_{self._graph_description}"
+
+    @property
+    def directory(self) -> str:
+        return os.path.join(super().directory, self.graph_name)
+
+    def node_logger(
+        self, node_description: str, node_commit_id: int | None = None
+    ) -> NodeDataLogger:
+        return NodeDataLogger(
+            self._param_db,
+            self._log_directory,
+            self._graph_description,
+            node_description,
+            self._graph_commit_id,
+            node_commit_id,
+        )
+
+
+class NodeDataLogger(GraphDataLogger):
+    def __init__(
+        self,
+        param_db: ParamDB[Any],
+        log_directory: str,
+        graph_description: str,
+        node_description: str,
+        graph_commit_id: int | None = None,
+        node_commit_id: int | None = None,
+    ) -> None:
+        self._node_description = node_description
+        self._node_commit_id = self._commit_id_or_latest(param_db, node_commit_id)
+        super().__init__(param_db, log_directory, graph_description, graph_commit_id)
+
+    @property
+    def node_name(self) -> str:
+        return f"node_{self._node_commit_id}_{self._node_description}"
+
+    @property
+    def directory(self) -> str:
+        return os.path.join(super().directory, self.node_name)
 
     def _log_filename(
-        self, graph: str, node: str, log_type: str, commit_id: int
+        self, log_type: str, log_description: str, log_commit_id: int
     ) -> str:
-        node_directory = self.node_directory(graph, node)
-        os.makedirs(node_directory, exist_ok=True)
-        return os.path.join(node_directory, f"{log_type}_{commit_id}.nc")
-
-    def node_directory(self, graph: str, node: str) -> str:
-        """Directory path for the given graph and node."""
-        return os.path.join(self.log_directory, graph, node)
+        return os.path.join(
+            self.directory, f"{log_commit_id}_{log_type}_{log_description}.nc"
+        )
 
     def log(
-        self,
-        graph: str,
-        node: str,
-        analysis: bool = False,
-        commit_id: int | None = None,
-    ) -> None:
+        self, log_description: str, analysis: bool = False, commit_id: int | None = None
+    ) -> xr.Dataset:
         """
         Log the given experiment or analysis data for the given graph and node.
 
@@ -43,19 +129,24 @@ class DataLogger:
         ID of the latest commit if not).
         """
         log_type = "analysis" if analysis else "data"
-        if commit_id is None:
-            latest_commit = self.param_db.latest_commit
-            if latest_commit is None:
-                raise IndexError(
-                    "cannot tag the log with the most recent commit because database"
-                    f" {self.param_db.path} is empty"
-                )
-            commit_id = latest_commit.id
-        data = xr.Dataset()
-        log_filename = self._log_filename(graph, node, log_type, commit_id)
+        commit_id = self._commit_id_or_latest(self._param_db, commit_id)
+        dataset = xr.Dataset(
+            attrs={
+                "graph": self.graph_name,
+                "node": self.node_name,
+                "log_type": log_type,
+                "paramdb_path": os.path.abspath(self._param_db.path),
+                "paramdb_commit_id": commit_id,
+                "created_timestamp": time.time(),
+            },
+        )
+        # if not os.path.exists(self.directory):
+        #     os.makedirs(self.directory)
+        log_filename = self._log_filename(log_type, log_description, commit_id)
         if os.path.exists(log_filename):
             raise FileExistsError(
-                f"{log_type} log for graph {graph}, node {node}, commit {commit_id}"
-                f" already exists"
+                f"{log_type} log '{log_description}' for {self.graph_name},"
+                f" {self.node_name}, commit {commit_id} already exists"
             )
-        data.to_netcdf(log_filename)
+        dataset.to_netcdf(log_filename)
+        return dataset
--- examples/log_data.py
+++ examples/log_data.py
@@ -3,7 +3,7 @@
 from paramdb import ParamDB
 from datalogger import DataLogger
 
-param_db = ParamDB[int](":memory:")
+param_db = ParamDB[int]("qpu.db")
 data_logger = DataLogger(param_db, "data_logs")
 
 param_db.commit("Commit 1", 101)
@@ -12,4 +12,10 @@ param_db.commit("Commit 3", 103)
 param_db.commit("Commit 4", 104)
 param_db.commit("Commit 5", 105)
 
-data_logger.log(graph="cross_entropy", node="rabi_calibration")
+for i in range(5):
+    graph_data_logger = data_logger.graph_logger("cross_entropy")
+    for j in range(5):
+        node_data_logger = graph_data_logger.node_logger("rabi_calibration")
+        param_db.commit("Commit", 10 * i + j)
+        node_data_logger.log("rabi")
+        node_data_logger.log("rabi_computed", analysis=True)
